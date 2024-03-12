f4c4ca07794f601c797b4c87f34e3e2d462a33af
Pedro Morillas Alcaide <pdmorillas@ugr.es>
2024-03-12 20:40:33 +0100

    Adjusting environment variables

--- src/main/java/com/rok/faker/performanceapirest/entitys/Player.java
+++ src/main/java/com/rok/faker/performanceapirest/entitys/Player.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
@@ -17,6 +18,7 @@ public class Player {
     private String name;
 
     @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime initDate;
 
     private int vip;
